30a5339f028eea2bcf7590f22f760dc78f95e2b9
Juan Alberto Aranda Alvarez <jaranda@a3nstudio.com>
2020-02-11 04:33:06 -0600

    Section 01 Challenge
    "Hello Juan!" messge

--- Hello World/Assets/HelloWorld.cs
+++ Hello World/Assets/HelloWorld.cs
@@ -7,7 +7,7 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Hellow World");
+        print("Hellow Juan! Welcome to the machine!");
     }
 
     // Update is called once per frame
